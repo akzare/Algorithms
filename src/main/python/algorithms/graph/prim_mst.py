4bf2f0ce8bbc42a3e43ba8f2f57fe199dc3ee0ff
Dharaneedharan <ddsss1999@gmail.com>
2021-10-22 12:22:50 +0530

    Add driver code and test with input

--- src/main/python/algorithms/graph/prim_mst.py
+++ src/main/python/algorithms/graph/prim_mst.py
@@ -58,3 +58,15 @@ class Graph:
                     parent[v] = u
 
         self.printMST(parent)
+
+g = Graph(5)
+
+g.graph = [
+    [0, 2, 0, 6, 0],
+    [2, 0, 3, 8, 5],
+    [0, 3, 0, 0, 7],
+    [6, 8, 0, 0, 9],
+    [0, 5, 7, 9, 0],
+]
+
+g.primMST() 
