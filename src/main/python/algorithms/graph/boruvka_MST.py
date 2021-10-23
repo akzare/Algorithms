eca9efe069500a05c5b55e2a78edcefa116c31b0
Dharaneedharan <ddsss1999@gmail.com>
2021-10-23 08:00:06 +0530

    Add driver code with test input

--- src/main/python/algorithms/graph/boruvka_MST.py
+++ src/main/python/algorithms/graph/boruvka_MST.py
@@ -75,3 +75,12 @@ class Graph:
 			cheapest =[-1] * self.V
 
 		print ("Weight of MST is %d" % MSTweight)
+		
+g = Graph(4)
+g.addEdge(0, 1, 10)
+g.addEdge(0, 2, 6)
+g.addEdge(0, 3, 5)
+g.addEdge(1, 3, 15)
+g.addEdge(2, 3, 4)
+
+g.boruvkaMST()
