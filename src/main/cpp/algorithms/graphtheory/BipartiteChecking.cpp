05373eccaab23c6dda9f346ad2ada0ea4a52ada4
Visrutha <visruthabalashanmugam@gmail.com>
2021-11-23 11:47:58 +0530

    Added Boruvka Minimum spanning tree

--- src/main/cpp/algorithms/graphtheory/BipartiteChecking.cpp
+++ src/main/cpp/algorithms/graphtheory/BipartiteChecking.cpp
@@ -1,56 +1,78 @@
+
 #include <iostream>
 #include <queue>
+#define V 4
 
 using namespace std;
 
-int bipartite(int g[][10], int s, int n)
+bool isBipartite(int G[][V], int src)
 {
-    int *col = new int [n];
+	int colorArr[V];
+	for (int i = 0; i < V; ++i)
+		colorArr[i] = -1;
 
-    for (int i = 0; i < n; i++)
-        col[i] = -1;
+	// Assign first color to source
+	colorArr[src] = 1;
 
-    queue<int> q;
+	// Create a queue (FIFO) of vertex
+	// numbers and enqueue source vertex
+	// for BFS traversal
+	queue <int> q;
+	q.push(src);
 
-    col[s] = 1;
-    q.push(s);
+	// Run while there are vertices
+	// in queue (Similar to BFS)
+	while (!q.empty())
+	{
+		// Dequeue a vertex from queue ( Refer http://goo.gl/35oz8 )
+		int u = q.front();
+		q.pop();
 
-    while (!q.empty())
-    {
-        int u = q.front();
-        q.pop();
+		// Return false if there is a self-loop
+		if (G[u][u] == 1)
+		return false;
 
-        if (g[u][u] == 1)
-            return 0;
+		// Find all non-colored adjacent vertices
+		for (int v = 0; v < V; ++v)
+		{
+			// An edge from u to v exists and
+			// destination v is not colored
+			if (G[u][v] && colorArr[v] == -1)
+			{
+				// Assign alternate color to this adjacent v of u
+				colorArr[v] = 1 - colorArr[u];
+				q.push(v);
+			}
 
-        for (int v = 0; v < n; v++)
-        {
-            if (g[u][v] && col[v] == -1)
-            {
-                col[v] = 1 - col[u];
-                q.push(v);
-            }
-            else if (g[u][v] == 1 && col[u] == col[v])
-                return 0;
-        }
-    }
+			// An edge from u to v exists and destination
+			// v is colored with same color as u
+			else if (G[u][v] && colorArr[v] == colorArr[u])
+				return false;
+		}
+	}
 
-    return 1;
+	// If we reach here, then all adjacent
+	// vertices can be colored with alternate color
+	return true;
 }
 
-
+// Driver program to test above function
 int main()
 {
-    int g[10][10], n;
+	int G[][V] = {{0, 1, 0, 1},
+		{1, 0, 1, 0},
+		{0, 1, 0, 1},
+		{1, 0, 1, 0}
+	};
 
-    cin >> n;
+	isBipartite(G, 0) ? cout << "Yes\n" : cout << "No\n";
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            cin >> g[i][j];
+		int GN[][V] = {{1, 1, 0, 1},
+		{1, 0, 1, 1},
+		{0, 1, 0, 1},
+		{1, 0, 1, 0}
+	};
 
-    int x = bipartite(g, 0, n);
-
-    cout << x << endl;
-    return 0;
+	isBipartite(GN, 0) ? cout << "Yes\n" : cout << "No\n";
+	return 0;
 }
